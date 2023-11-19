2428095d5c1c8fa761100b173709361e6d9a1514
Marv.Ghinux.Code <marvellousadeyemi5@gmail.com>
2023-11-19 17:11:19 +0100

    Update swap_add.c

--- swap_add.c
+++ swap_add.c
@@ -3,16 +3,16 @@
 /**
   *swap_node -  Swaps the top two elements of the stack.
   *@stack: A double pointer to the top of the stack
-  *@line_num: The line number in the Monty bytecode file
+  *@line_number: The line number in the Monty bytecode file
   *where the push operation is called
   */
-void swap_node(stack_t **stack, unsigned int line_num)
+void swap_node(stack_t **stack, unsigned int line_number)
 {
 	int temp;
 
 	if(!stack || !*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line_num);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
@@ -24,7 +24,7 @@ void swap_node(stack_t **stack, unsigned int line_num)
 /**
   *add_node -  Adds the top two elements of the stack.
   *@stack: A pointer to the top of the stack.
-  *@line_num: The line number in the monty Bytecode file
+  *@line_number: The line number in the monty Bytecode file
   *where the push operation is called.
   */
 void add_node(stack_t **stack, unsigned int line_number)
