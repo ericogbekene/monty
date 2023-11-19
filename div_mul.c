c4c7d5dc92bb500702327021d2a75f9228b3e133
Eric Ogbekene <ericogbekene@icloud.com>
2023-11-19 13:00:57 -0800

    err: stderror fix

--- div_mul.c
+++ div_mul.c
@@ -45,7 +45,7 @@ void mul_node(stack_t **stack, unsigned int line_number)
 	/*Check if the stack contains less than two elements */
 	if (!stack || !*stack || !(*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
